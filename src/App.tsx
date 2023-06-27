391dcf74ef134a987a72ba2bdb5facbaaccbb5d7
hans000 <hhance@163.com>
2023-06-27 22:39:30 +0800

    feat: supprot jar

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { AppContext, useAppReducer } from "./store"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
 import './index.less'
-import { Spin } from "antd"
+import { Spin, Upload } from "antd"
 import Board from "./components/Board"
 
 function getBg(str: string) {
@@ -17,6 +17,36 @@ function getBg(str: string) {
 }
 
 const lineColorArray = ['#345', '#345', '#f6f6f6', '#f6f6f6', '#f6f6f6']
+
+function formatFile(file: jszip.JSZipObject) {
+    return file.async('string').then(res => {
+        try {
+            const isMatch = /^data\/(.+)\/advancements/.test(file.name)
+            return {
+                ...JSON.parse(res),
+                name: isMatch ? file.name.replace(/^data\/(.+)\/advancements\//, '$1:').replace(/\.json$/, '') : 'minecraft:' + file.name.replace(/\.json$/, '')
+            }
+        } catch (error) {
+            return {
+                name: '$',
+            }
+        }
+    })
+}
+
+function buildData(res: any[]) {
+    const data = []
+    data.push({ name: '$', })
+    res.forEach(item => {
+        if (item.display) {
+            data.push(item)
+        }
+    })
+    return d3.stratify()
+        .id((d: any) => d.name)
+        .parentId((d: any) => d.name !== '$' ? (d.parent || '$') : undefined)
+        (data)
+}
 
 export default function App() {
     const [state, dispatch] = useAppReducer()
@@ -38,62 +68,28 @@ export default function App() {
     }, [state.version])
 
     useEffect(() => {
-        dispatch({
-            type: 'UpdateLoading',
-            payload: true,
-        })
+        dispatch({ type: 'UpdateLoading', payload: true })
         fetch(`https://unpkg.com/@wikijs/mc-advancements/dist/${state.version}.zip`)
             .then(res => res.blob())
             .then(res => jszip.loadAsync(res))
-            .then(res => {
-                return Promise.all(
-                    res.filter((_, file) => !file.dir)
-                        .map(
-                            (file) => file.async('string').then(res => {
-                                try {
-                                    return {
-                                        ...JSON.parse(res),
-                                        name: 'minecraft:' + file.name.slice(0, -5),
-                                    }
-                                } catch (error) {
-                                    return {
-                                        name: '$',
-                                    }
-                                }
-                            })
-                        )
-                )
-            })
-            .then(res => {
-                const data = []
-                data.push({ name: '$', })
-                res.forEach(item => {
-                    if (item.display) {
-                        data.push(item)
-                    }
-                })
-                rootRef.current = d3.stratify()
-                    .id((d: any) => d.name)
-                    .parentId((d: any) => d.name !== '$' ? (d.parent || '$') : undefined)
-                    (data)
-                const children = rootRef.current.children
-                setActive(0)
-                const actNode = children[0].copy()
-                setActiveNode(actNode)
-                dispatch({
-                    type: 'UpdateBg',
-                    payload: getBg(actNode.data.display.background),
-                })
-                setTabs(children.map(node => node.data))
-            }).catch(() => {
+            .then(res => Promise.all(res.filter((_, file) => !file.dir).map(formatFile)))
+            .then(update)
+            .catch(() => {
                 alert('资源加载失败')
             }).finally(() => {
-                dispatch({
-                    type: 'UpdateLoading',
-                    payload: false,
-                })
+                dispatch({ type: 'UpdateLoading', payload: false })
             })
     }, [state.version])
+
+    function update(res) {
+        rootRef.current = buildData(res)
+        const children = rootRef.current.children
+        setActive(0)
+        const actNode = children[0].copy()
+        setActiveNode(actNode)
+        dispatch({ type: 'UpdateBg', payload: getBg(actNode.data.display.background) })
+        setTabs(children.map(node => node.data))
+    }
 
     return (
         <AppContext.Provider value={[state, dispatch]}>
@@ -114,6 +110,25 @@ export default function App() {
                         </Panel>
                     </div>
                     <Board />
+                    <div style={{ maxWidth: 640, margin: '32px auto' }}>
+                        <Upload.Dragger fileList={[]} style={{ marginBottom: 32 }}
+                            beforeUpload={(rcfile) => {
+                                dispatch({ type: 'UpdateLoading', payload: true })
+                                const handleFilter = (file: jszip.JSZipObject) => !file.dir && file.name.startsWith('data/minecraft/advancements')
+                                jszip.loadAsync(rcfile.arrayBuffer())
+                                    .then(res => Promise.all(res.filter((_, file) => handleFilter(file)).map(formatFile)))
+                                    .then(update)
+                                    .catch((err) => {
+                                        console.log(err)
+                                        alert('资源加载失败')
+                                    }).finally(() => {
+                                        dispatch({ type: 'UpdateLoading', payload: false })
+                                    })
+                                return false
+                            }}>
+                            <span>使用离线数据（上传versions目录的jar包）</span>
+                        </Upload.Dragger>
+                    </div>
                     <Footer />
                 </div>
             </Spin>
