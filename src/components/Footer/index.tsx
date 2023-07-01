8bf37af498c6deec8d9d0ba4421189f05f513db7
hans000 <hhance@163.com>
2023-07-01 13:50:17 +0800

    feat: update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
     return (
         <footer>
             <span>Copyright © {new Date().getFullYear()} by </span>
-            <a style={{ textDecoration: 'none' }} href="https://github.com/hans000/mc-advancement-viewer" target="_blank">hans000</a>
+            <a style={{ textDecoration: 'none' }} href="https://github.com/mc-favorite-tools/advancement-viewer" target="_blank">hans000</a>
             <span style={{ marginLeft: 12 }}>QQ: 2112717288</span>
         </footer>
     )
