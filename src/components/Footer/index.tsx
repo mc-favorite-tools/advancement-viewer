c74ef2ba0be4782adcb126b5b76cbf9a0d280b35
hans000 <hhance@163.com>
2023-07-29 09:52:23 +0800

    feat: add page viewer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -9,6 +9,7 @@ export default function Footer() {
             <span>Copyright © {new Date().getFullYear()} by </span>
             <a style={{ textDecoration: 'none' }} href="https://github.com/mc-favorite-tools/advancement-viewer" target="_blank">hans000</a>
             <span style={{ marginLeft: 12 }}>QQ: 2112717288</span>
+            <img style={{ transform: 'scale(.8)', transformOrigin: 'bottom' }} src="https://badges.toozhao.com/badges/01H6FNF5Z802Z9SC7YVXQPP69S/green.svg" />
         </footer>
     )
 }
